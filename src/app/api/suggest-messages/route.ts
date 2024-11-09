414150c985f4719882bab991f7cc8524abaaf902
Zaryab <zaryabimran222@gmail.com>
2024-11-09 10:33:19 +0500

    fixed ai errors

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,38 +1,59 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { NextResponse } from 'next/server';
+// import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
+import { streamText, convertToCoreMessages } from "ai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-export const runtime = 'edge';
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   try {
     const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+      "create a list of three open-ended questions formatting as a single string. Each question should be seperated by '||' These questions are for social anonymous messaging platform like Qooh.me and should be suitable for diverse audience. Avoid personal or sensitive topics, focusing instead of personal themes that encourage friendly interactions. For Example, your output should be structured like this: 'What's your hobby you'v recently started?||If you have a dinner with any historical figure then who would it be?||What's simple thing that make you happy?'. Ensure that the questions are intriguing, foster curiosity and contribute to positive and welcoming conversational environment.";
 
-    const response = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      max_tokens: 400,
-      stream: true,
+    const result = await streamText({
+      model: google("gemini-1.5-pro-latest"),
       prompt,
     });
 
-    const stream = OpenAIStream(response);
-    
-    
-    return new StreamingTextResponse(stream);
+    return result.toDataStreamResponse();
   } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      // OpenAI API error handling
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
-    } else {
-      // General error handling
-      console.error('An unexpected error occurred:', error);
-      throw error;
-    }
+    console.log("Error in generating ai");
   }
 }
+
+// import OpenAI from 'openai';
+// import { OpenAIStream, StreamingTextResponse } from 'ai';
+// import { NextResponse } from 'next/server';
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// export const runtime = 'edge';
+
+// export async function POST(req: Request) {
+//   try {
+//     const prompt =
+//       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+//     const response = await openai.completions.create({
+//       model: 'gpt-3.5-turbo-instruct',
+//       max_tokens: 400,
+//       stream: true,
+//       prompt,
+//     });
+
+//     const stream = OpenAIStream(response);
+
+//     return new StreamingTextResponse(stream);
+//   } catch (error) {
+//     if (error instanceof OpenAI.APIError) {
+//       // OpenAI API error handling
+//       const { name, status, headers, message } = error;
+//       return NextResponse.json({ name, status, headers, message }, { status });
+//     } else {
+//       // General error handling
+//       console.error('An unexpected error occurred:', error);
+//       throw error;
+//     }
+//   }
+// }
