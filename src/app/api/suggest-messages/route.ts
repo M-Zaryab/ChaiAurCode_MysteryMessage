5f1136989d85176e0ca9401382def8b0c624a446
Zaryab <zaryabimran222@gmail.com>
2024-11-09 10:52:59 +0500

    fixed production errors

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -2,7 +2,6 @@
 import { google } from "@ai-sdk/google";
 import { streamText } from "ai";
 
-export const dynamic = "force-dynamic";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
